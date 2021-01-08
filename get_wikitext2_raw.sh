39d9bb4a0fe6fcda7dbe3172ce9120d0933288b1
Edin Ikanovic <edin@ogtal.dk>
2021-01-08 10:22:49 +0100

    rm zip file

--- get_wikitext2_raw.sh
+++ get_wikitext2_raw.sh
@@ -3,3 +3,4 @@ wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-raw-v1.zi
 unzip wikitext-2-raw-v1.zip
 cd wikitext-2-raw
 mv wiki.test.raw test && mv wiki.train.raw train && mv wiki.valid.raw valid
+rm wikitext-2-raw-v1.zip
